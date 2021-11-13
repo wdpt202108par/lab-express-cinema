3861c0412fc92ecd5615d417986ea61857eee5de
aude.claquesin <aude.claquesin@gmail.com>
2021-11-13 15:46:37 +0100

    It 1

--- lab-express-cinema/db/index.js
+++ lab-express-cinema/db/index.js
@@ -8,16 +8,12 @@ const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-cinema";
 
 mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  })
+  .connect(MONGO_URI)
   .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
   });
-  
