4a4f7ca7ac7d1fda02eb24950a8423d64ab41bf2
kthomas <kevin.allan.thomas@gmail.com>
2021-11-13 17:14:08 +0100

    Iteration 1 & 2, Iteration 3 in progress

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,13 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res, next) => { 
+    console.log('page movie affichée');
+    Movie.find()
+    .then(function (movieFromDb){
+        res.render('movies', {
+            mymovies: movieFromDb
+        })
+    });
+
 module.exports = router;
