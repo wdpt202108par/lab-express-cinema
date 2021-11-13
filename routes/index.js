f614b2d811babb2189f56331bc534941036f79b7
Silas Paul <silas.paul@outlook.fr>
2021-11-13 16:22:28 +0100

    iteration 0à4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
+const Movie = require('../models/Movie.model');
 const express = require('express');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+router.get('/movies',function (req, res, next) {
+    Movie.find()
+        .then(function(moviesFromDb){
+            res.render('movies',{
+                movies: moviesFromDb
+            })
+        })
+    .catch(err => console.log(err));
+})  
+
+router.get('/movies/:id',function (req, res, next){
+    Movie.findById(req.params.id)
+        .then(function(movieFromDb){
+            res.render('movie-details',{
+                movie: movieFromDb
+            })
+        })
+        .catch(err => console.log(err));
+})
 
 module.exports = router;
