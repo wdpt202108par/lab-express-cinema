12f208fd0effaa99c9bbdade77e5c6bfd3c69dc4
Emma HB <emmanuelle.bolognini@gmail.com>
2021-11-15 17:20:37 +0100

    LAB with Bonus done

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model');
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+//Get 'Movies Page'
 router.get('/movies', (req, res, next) => { 
-    console.log('page movie affichée');
-    Movie.find()
-    .then(function (movieFromDb){
-        res.render('movies', {
-            mymovies: movieFromDb
-        })
-    });
+  Movie.find()
+  .then(function (moviesFromDb) {
+    res.render('movies', {
+      mymovies: moviesFromDb
+    })
+  })
+  .catch(err => console.log(err))
+})
+
+//Get 'Movie Details Page'
+router.get('/movies/:movieId', (req, res, next) => { 
+    Movie.findById(req.params.movieId)
+    .then(function (movieFromDb) {
+      res.render('movie-details', {
+        mymovie: movieFromDb
+      })
+    })
+    .catch(err => console.log(err))
+  })
 
 module.exports = router;
